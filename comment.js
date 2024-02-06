c7134de4cb5bb0cee03b6d501ea4497f9d65bc70
Vinicius Camilotti <vinicamilotti@gmail.com>
2024-02-06 13:31:19 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,15 +1,62 @@
 //create a webserver
 var http = require('http');
 var fs = require('fs');
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var url = req.url;
-    if(url === '/'){
-        fs.createReadStream(__dirname + '/index.html').pipe(res);
-    }else if(url === '/comment'){
-        fs.createReadStream(__dirname + '/comment.json').pipe(res);
-    }else{
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/404.html').pipe(res);
-    }
-}).listen(1337);
+var url = require('url');
+var path = require('path');
+
+var server = http.createServer(function(req, res){
+    var pathname = url.parse(req.url).pathname;
+    var realPath = path.join("public", pathname);
+    var ext = path.extname(realPath);
+    ext = ext ? ext.slice(1) : 'unknown';
+    fs.exists(realPath, function(exists){
+        if(!exists){
+            res.writeHead(404, {
+            'Content-Type': 'text/plain'
+        });
+            res.write("This request URL " + pathname + " was not found on this server.");
+            res.end();
+        } else {
+            fs.readFile(realPath, "binary", function(err, file){
+                if(err){
+                    res.writeHead(500, {
+                        'Content-Type': 'text/plain'
+                    });
+                    res.end(err);
+                } else {
+                    var contentType = mine[ext] || "text/plain";
+                    res.writeHead(200, {
+                        'Content-Type': contentType
+                    });
+                    res.write(file, "binary");
+                    res.end();
+                }
+            });
+        }
+    });
+});
+
+server.listen(3000, function(){
+    console.log("Server is listening on 3000");
+});
+
+var mine = {
+    "css": "text/css",
+    "gif": "image/gif",
+    "html": "text/html",
+    "ico": "image/x-icon",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "js": "text/javascript",
+    "json": "application/json",
+    "pdf": "application/pdf",
+    "png": "image/png",
+    "svg": "image/svg+xml",
+    "swf": "application/x-shockwave-flash",
+    "tiff": "image/tiff",
+    "txt": "text/plain",
+    "wav": "audio/x-wav",
+    "wma": "audio/x-ms-wma",
+    "wmv": "video/x-ms-wmv",
+    "xml": "text/xml"
+};
